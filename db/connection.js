fd3378191a563c21b0efc6beb43fcb685b9232aa
Roman Ayyaz <romanayaz7@gmail.com>
2024-03-01 02:58:27 +0500

    Blog App

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
-const uri = "mongodb+srv://romanayaz7:1234@cluster0.h31qcuh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-mongoose.connect(uri).then(()=>{
+require('dotenv').config()
+//const uri = "mongodb+srv://romanayaz7:1234@cluster0.h31qcuh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+mongoose.connect(process.env.MONGODB_URI).then(()=>{
     console.log('Connected to Database Successfully')
 }).catch((err)=>{
     console.log(err)
